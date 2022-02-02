8d1d057aa4962478e514b98b01bc964843609d05
Faza <firdausauliafaza@gmail.com>
2022-02-02 14:50:35 +0700

    permission + role

---
+++ app/Models/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * guarded
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * category
+     *
+     * @return void
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * getImageAttribute
+     *
+     * @param  mixed $image
+     * @return void
+     */
+    public function getImageAttribute($image)
+    {
+        return asset('storage/posts/' . $image);
+    }
+}
